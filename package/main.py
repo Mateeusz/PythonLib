b3dfc583adfb9e40678f4b967fc7abdc0f6df5c6
Mateusz <mateuszharazin@wp.pl>
2019-05-22 20:53:57 +0200

    1st commit

---
+++ package/main.py
@@ -0,0 +1,55 @@
+
+class Item():
+
+    name = "name";
+    quantity = 0;
+
+    def __init__(self, _name, _quantity):
+        self.name = _name
+        self.quantity = _quantity
+
+    def __str__(self):
+        print("Produkt: ", self.name , " Ilość: ", self.quantity + " sztuk.")
+
+
+class Generator():
+
+    partyType = ["urodziny", "kolacja", "obiad"]
+    productList =[]
+    formFile = []
+    parameters = ["nie określono","1(domyślnie)"]
+
+    def loadFromFile(self, name):
+        with open(name) as f:
+            self.formFile = f.readlines()
+
+            self.formFile = [x.strip() for x in self.formFile]
+
+    def generateList(self, _partyType):
+        #print(self.formFile, _partyType, self.formFile.__len__())
+        temp = self.formFile.__len__()
+        self.parameters[0] = _partyType
+
+        for i in range (self.formFile.__len__()):
+            # print(i)
+
+            if(self.formFile[i] == _partyType):
+                print("Typ imprezy: ", self.formFile[i])
+                temp = i
+            if(i > temp and self.formFile[i] != "==="):
+                print(self.formFile[i])
+                item = Item(self.formFile[i].split( )[0], self.formFile[i].split( )[1])
+                self.productList.append(item)
+
+    def refreshQuantity(self, guestNumber):
+        self.parameters[1] = guestNumber
+        for i in range(self.productList.__len__()):
+            self.productList[i].quantity = int(self.productList[i].quantity)*int(guestNumber)
+
+    def printList(self):
+        print("Typ imprezy:",self.parameters[0],", Ilość gości:",self.parameters[1])
+        for i in range(self.productList.__len__()):
+            print("Produkt:", self.productList[i].name, ", Ilość:", self.productList[i].quantity, "szt.")
+
+
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dppPackage',
+    version='0.1',
+    packages=['package'],
+    license='MIT',
+    description='Moja biblioteka',
+    #long_description=open('README.txt').read(),
+    url='https://git.e-science.pl/mharazin238652/ShoppingListPythonLibrary.git',
+    author='Mateusz Harazin',
+    author_email='mharazin238652@e-science.pl'
+)
